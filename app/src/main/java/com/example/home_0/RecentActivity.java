cefc4e533f71f78e393f4c48949065e5cc2ed567
seongjoow <seongjoow@gmail.com>
2022-03-26 15:51:52 +0900

    1st commit colors tbd

---
+++ app/src/main/java/com/example/home_0/RecentActivity.java
@@ -0,0 +1,14 @@
+package com.example.home_0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recent);
+    }
+}
---
+++ app/src/main/java/com/example/home_0/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.example.home_0;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.car.ui.toolbar.SearchView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
